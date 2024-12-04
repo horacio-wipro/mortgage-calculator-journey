eec7fee0992566393fa5a69e84f87acd4422b1a5
horacio-wipro <horacio.lopez@wipro.com>
2024-12-04 15:12:49 +0000

    fixes for screen 4

--- src/pages/MortgageScreen4Final.tsx
+++ src/pages/MortgageScreen4Final.tsx
@@ -2,9 +2,10 @@ import { FunctionComponent } from "react";
 import { useNavigate } from "react-router-dom";
 import Back from "../components/Back";
 import Button from "../components/Button";
-import Container1 from "../components/Container1";
 import Footer from "../components/Footer";
+import FormQuestion from "../components/FormQuestion";
 import GloablNav from "../components/GloablNav";
+import TitleAndContent from "../components/TitleAndContent";
 import styles from "./MortgageScreen4Final.module.css";
 
 const MortgageScreen4Final: FunctionComponent = () => {
@@ -24,7 +25,38 @@ const MortgageScreen4Final: FunctionComponent = () => {
             sVG="/svg-2.svg"
             onClick={() => navigate("/mortgage-screen-3-final")}
           />
-          <Container1 />
+          <hr className={styles.horizontalRule} />
+          <TitleAndContent
+            className={styles.titleandcontent}
+            title="Your outgoings"
+            headerText="Mortgage Calculator"
+          />
+          <FormQuestion
+            className={styles.formquestion}
+            isHorizontal
+            question="How many people rely on your income?"
+            explanation="These can be children or adults of any age."
+            questions={["0", "1", "2", "3 or more"]}
+            error="Let us know how many people rely on your income."
+          />
+          <FormQuestion
+            className={styles.formquestion1}
+            isNumberInput
+            question="Do you have any credit card balances?"
+            explanation="If so, please tell us the total amount outstanding."
+          />
+          <FormQuestion
+            className={styles.formquestion1}
+            isNumberInput
+            question="Do you have any loans you pay back monthly?"
+            explanation="If so, please tell us the total amount you pay each month."
+          />
+          <FormQuestion
+            className={styles.formquestion3}
+            isNumberInput
+            question="Do you have any other monthly expenses?"
+            explanation="If so, please tell us how much you pay each month for things like childcare costs, electrical items, or furniture."
+          />
           <div className={styles.backParent}>
             <Back
               iconRight={false}
